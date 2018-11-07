83abb45c5f54dbf7a6d9b86f6d9e833749425b37
Antonín Simerský <antonin.simersky@live.com>
2018-11-07 08:58:08 +0100

    Data fetch

---
+++ src/DataFetch.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class DataFetch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      data: {}
+    };
+  }
+
+  handleFetch = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const [item] = data.results;
+    this.setState({ data: item, loading: false });
+  }
+
+  componentDidMount() {
+    this.handleFetch("https://api.randomuser.me/");
+  }
+
+  render() {
+    const { loading, data } = this.state;
+    return (
+      <div>
+        <h3>Data fetch:</h3>
+        <ul>
+          {loading ? <div>...loading</div> : <div>First Name: {data.name.first} <br /> Last Name: {data.name.last}</div>}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default DataFetch;
